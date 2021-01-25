4d4288cae60c26e9e9ccb4abddac6c6caa433bfe
Mohamad-Aboda <sirmohamedabdelhamid11@gmail.com>
2021-01-25 16:42:09 +0200

    create home and about routes but unable to load pics + css files

---
+++ AgriolaProject/newsapp/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import path
+
+from .views import home, about 
+urlpatterns = [
+    path('',home, name = 'home'),
+    path('about/', about, name='about'),  
+]
---
+++ AgriolaProject/static/buildREADME.md
@@ -0,0 +1,2 @@
+# Agriola-web
+in this folder we should have images , scripts ,styles ,fonts and flyer directores.
