b4b2e71da50c6e6ed5c6cfaac63f4fb7bb591779
Omar Kassem <62248851+0oM4R@users.noreply.github.com>
2021-04-20 00:26:16 +0200

    Delete AgriolaProject directory

--- AgriolaProject/newsapp/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-
-from .views import home, about 
-urlpatterns = [
-    path('',home, name = 'home'),
-    path('about/', about, name='about'),  
-]
--- AgriolaProject/static/buildREADME.md
+++
@@ -1,2 +0,0 @@
-# Agriola-web
-in this folder we should have images , scripts ,styles ,fonts and flyer directores.
